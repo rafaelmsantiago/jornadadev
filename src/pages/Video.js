a86ada62240418ea089779317f523a0e08b64e15
Rafael Marin Santiago <rafael.santiago@mte-thomson.com.br>
2023-03-30 08:32:27 -0300

    Componente Video

---
+++ src/pages/Video.js
@@ -0,0 +1,31 @@
+import React, { useRef, useState } from "react";
+import "./Video.css";
+
+function Video() {
+  const videoRef = useRef(null);
+  const [play, setPlay] = useState(false);
+
+  function handdleStart() {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
+    }
+  }
+
+  return (
+    <div className="video">
+      <video
+        className="video__player"
+        ref={videoRef}
+        onClick={handdleStart}
+        loop
+        src="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/brecker2.mp4?alt=media&token=d1a44acd-bef3-4b18-bafe-92fa0b26828a"
+      ></video>
+    </div>
+  );
+}
+
+export default Video;
